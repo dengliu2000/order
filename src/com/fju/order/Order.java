e170448408dc55c149f76e152c7833867ba1e06e
dengliu2000 <dengliu2000@gmail.com>
2022-04-29 16:15:01 +0800

    order_finished

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,13 +3,12 @@ package com.fju.order;
 public class Order{
     int amount;
     Delivery delivery;
-    Delivery price;
     int shipFeed;
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+        shipFeed=delivery.price;
     }
-    
     public int total(){
         return amount+ shipFeed;
     }
